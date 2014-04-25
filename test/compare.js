178f2a6e2dee44268bdcf738568635cbf0db8b86
Evgeniy Gavrushin <job.egavr@yandex.ru>
2014-04-25 18:04:02 +0300

    Added few real tests

---
+++ test/compare.js
@@ -0,0 +1,29 @@
+var path = require('path'),
+    fs = require('fs'),
+    compare = require(path.join('..', 'lib', 'html-differ')).compare;
+
+
+describe('\'compare\'', function () {
+
+    it('must be equal', function () {
+        var html1 = fs.readFileSync('test/basis/1.html');
+        var html2 = fs.readFileSync('test/basis/_1.html');
+
+        compare(html1, html2).must.be.true();
+    });
+
+    it('must be equal', function () {
+        var html1 = fs.readFileSync('test/basis/2.html');
+        var html2 = fs.readFileSync('test/basis/_2.html');
+
+        compare(html1, html2).must.be.true();
+    });
+
+    it('must be equal', function () {
+        var html1 = fs.readFileSync('test/basis/3.html');
+        var html2 = fs.readFileSync('test/basis/_3.html');
+
+        compare(html1, html2).must.be.true();
+    });
+
+});
