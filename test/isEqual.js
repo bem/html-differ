e0a94d7ea2012663bff23ddf4d9e9b297ef73e21
Evgeniy Gavryushin <job.egavr@yandex.ru>
2014-07-02 16:11:37 +0300

    Add 'BEM' preset

--- test/isEqual.js
+++ test/isEqual.js
@@ -1,5 +1,5 @@
 var fs = require('fs'),
-    htmlDiffer = require('../lib/html-differ');
+    HtmlDiffer = require('../lib/html-differ').HtmlDiffer;
 
 function readFiles(f1, f2) {
     var files = {};
@@ -13,51 +13,67 @@ function readFiles(f1, f2) {
 describe('\'isEqual\'', function () {
 
     it('must be equal', function () {
-        var files = readFiles('1.html', '_1.html');
+        var htmlDiffer = new HtmlDiffer(),
+
+            files = readFiles('1.html', '_1.html');
 
         htmlDiffer.isEqual(files.html1, files.html2).must.be.true();
     });
 
     it('must be not equal', function () {
-        var files = readFiles('2.html', '_2.html');
+        var htmlDiffer = new HtmlDiffer(),
+
+            files = readFiles('2.html', '_2.html');
 
         htmlDiffer.isEqual(files.html1, files.html2).must.be.false();
     });
 
     it('must ignore html attrs', function () {
-        var files = readFiles('3.html', '_3.html');
+        var htmlDiffer = new HtmlDiffer({ ignoreHtmlAttrs: ['id', 'for'] }),
 
-        htmlDiffer.isEqual(files.html1, files.html2, { ignoreHtmlAttrs: ['id', 'for'] } ).must.be.true();
+            files = readFiles('3.html', '_3.html');
+
+        htmlDiffer.isEqual(files.html1, files.html2).must.be.true();
     });
 
     it('must sort classes', function () {
-        var files = readFiles('4.html', '_4.html');
+        var htmlDiffer = new HtmlDiffer(),
+
+            files = readFiles('4.html', '_4.html');
 
         htmlDiffer.isEqual(files.html1, files.html2).must.be.true();
     });
 
     it('must sort attrs', function () {
-        var files = readFiles('5.html', '_5.html');
+        var htmlDiffer = new HtmlDiffer(),
+
+            files = readFiles('5.html', '_5.html');
 
         htmlDiffer.isEqual(files.html1, files.html2).must.be.true();
     });
 
     it('must sort content of attrs', function () {
-        var files = readFiles('6.html', '_6.html');
+        var htmlDiffer = new HtmlDiffer({ compareHtmlAttrsAsJSON: [ 'a', 'b' ] }),
 
-        htmlDiffer.isEqual(files.html1, files.html2, { compareHtmlAttrsAsJSON: [ 'a', 'b' ] } ).must.be.true();
+            files = readFiles('6.html', '_6.html');
+
+        htmlDiffer.isEqual(files.html1, files.html2).must.be.true();
     });
 
     it('must ignore space characters', function () {
-        var files = readFiles('7.html', '_7.html');
+        var htmlDiffer = new HtmlDiffer(),
+
+            files = readFiles('7.html', '_7.html');
 
         htmlDiffer.isEqual(files.html1, files.html2).must.be.true();
     });
 
     it('must sort \'onclick\' and \'ondblclick\' attrs content', function () {
-        var files = readFiles('8.html', '_8.html');
+        var htmlDiffer = new HtmlDiffer({ compareHtmlAttrsAsJSON: [ 'onclick', 'ondblclick' ] }),
 
-        htmlDiffer.isEqual(files.html1, files.html2, { compareHtmlAttrsAsJSON: [ 'onclick', 'ondblclick' ] } ).must.be.true();
+            files = readFiles('8.html', '_8.html');
+
+        htmlDiffer.isEqual(files.html1, files.html2).must.be.true();
     });
 
 });
