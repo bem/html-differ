3219cf6a674ce6f216b478b4c7ccd8da914ff178
Tony Brix <tony@brix.ninja>
2019-07-09 10:18:10 -0500

    remove vow from deps

--- test/differ/isEqual.js
+++ test/differ/isEqual.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const util = require('util');
+const readAsync = util.promisify(fs.readFile);
 const fixturesPath = path.join(__dirname, 'fixtures');
 const HtmlDiffer = require('../../lib/index').HtmlDiffer;
 
@@ -9,61 +11,67 @@ const HtmlDiffer = require('../../lib/index').HtmlDiffer;
  */
 function readFiles(basename) {
   const fileName = basename + '.html';
-
-  return {
-    html1: fs.readFileSync(path.join(fixturesPath, 'first', fileName), 'utf-8'),
-    html2: fs.readFileSync(path.join(fixturesPath, 'second', fileName), 'utf-8')
-  };
+  return Promise.all([
+    readAsync(path.join(fixturesPath, 'first', fileName), 'utf-8'),
+    readAsync(path.join(fixturesPath, 'second', fileName), 'utf-8')
+  ]);
 }
 
 describe('\'isEqual\'', function() {
   it('must be equal', async function() {
     const htmlDiffer = new HtmlDiffer();
-    const files = readFiles('equal');
+    const files = await readFiles('equal');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.true();
+    isEqual.must.be.true();
   });
 
   it('must be not equal', async function() {
     const htmlDiffer = new HtmlDiffer();
-    const files = readFiles('not-equal');
+    const files = await readFiles('not-equal');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.false();
+    isEqual.must.be.false();
   });
 
   it('must consider uppercase and lowercase declarations in \'doctype\' to be equal', async function() {
     const htmlDiffer = new HtmlDiffer();
-    const files = readFiles('doctype');
+    const files = await readFiles('doctype');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.true();
+    isEqual.must.be.true();
   });
 
   it('must sort attributes', async function() {
     const htmlDiffer = new HtmlDiffer();
-    const files = readFiles('sort-attributes');
+    const files = await readFiles('sort-attributes');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.true();
+    isEqual.must.be.true();
   });
 
   it('must sort classes\' values', async function() {
     const htmlDiffer = new HtmlDiffer();
-    const files = readFiles('sort-classes');
+    const files = await readFiles('sort-classes');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.true();
+    isEqual.must.be.true();
   });
 
   it('must sort values of attributes as JSON when the content is not a function', async function() {
     const htmlDiffer = new HtmlDiffer({ compareAttributesAsJSON: ['a', { name: 'b', isFunction: false }] });
-    const files = readFiles('sort-values-in-json-format');
+    const files = await readFiles('sort-values-in-json-format');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.true();
+    isEqual.must.be.true();
   });
 
   it('must handle invalid JSON', async function() {
     const htmlDiffer = new HtmlDiffer({ compareAttributesAsJSON: ['data-bem'] });
-    const files = readFiles('invalid-json');
+    const files = await readFiles('invalid-json');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.true();
+    isEqual.must.be.true();
   });
 
   it('must sort values of attributes as JSON when the content is a function', async function() {
@@ -74,80 +82,91 @@ describe('\'isEqual\'', function() {
       ]
     };
     const htmlDiffer = new HtmlDiffer(options);
-    const files = readFiles('sort-functions-in-json-format');
+    const files = await readFiles('sort-functions-in-json-format');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.true();
+    isEqual.must.be.true();
   });
 
   it('must handle invalid function', async function() {
     const options = { compareAttributesAsJSON: [{ name: 'onclick', isFunction: true }] };
     const htmlDiffer = new HtmlDiffer(options);
-    const files = readFiles('invalid-function');
+    const files = await readFiles('invalid-function');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.true();
+    isEqual.must.be.true();
   });
 
   it('must work option \'ignoreAttributes\'', async function() {
     const htmlDiffer = new HtmlDiffer({ ignoreAttributes: ['id', 'for'] });
-    const files = readFiles('ignore-attributes');
+    const files = await readFiles('ignore-attributes');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.true();
+    isEqual.must.be.true();
   });
 
   it('must work option \'ignoreWhitespaces\'', async function() {
     const htmlDiffer = new HtmlDiffer({ ignoreWhitespaces: true });
-    const files = readFiles('ignore-whitespaces');
+    const files = await readFiles('ignore-whitespaces');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.true();
+    isEqual.must.be.true();
   });
 
   it('must work option \'ignoreComments\'', async function() {
     const htmlDiffer = new HtmlDiffer();
-    const files = readFiles('ignore-comments');
+    const files = await readFiles('ignore-comments');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.true();
+    isEqual.must.be.true();
   });
 
   it('must does NOT work option \'ignoreComments\'', async function() {
     const htmlDiffer = new HtmlDiffer({ ignoreComments: false });
-    const files = readFiles('ignore-comments-false');
+    const files = await readFiles('ignore-comments-false');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.true();
+    isEqual.must.be.true();
   });
 
   it('must work option \'ignoreEndTags\'', async function() {
     const htmlDiffer = new HtmlDiffer({ ignoreEndTags: true });
-    const files = readFiles('ignore-end-tags');
+    const files = await readFiles('ignore-end-tags');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.true();
+    isEqual.must.be.true();
   });
 
   it('must work \'bem\' preset', async function() {
     const htmlDiffer = new HtmlDiffer('bem');
-    const files = readFiles('bem-preset');
+    const files = await readFiles('bem-preset');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.true();
+    isEqual.must.be.true();
   });
 
   it('must work mask {{RegExp}}', async function() {
     const htmlDiffer = new HtmlDiffer();
-    const files = readFiles('mask');
+    const files = await readFiles('mask');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.true();
+    isEqual.must.be.true();
   });
 
   it('must not be equal by mask {{RegExp}}', async function() {
     const htmlDiffer = new HtmlDiffer();
-    const files = readFiles('mask-false');
+    const files = await readFiles('mask-false');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.false();
+    isEqual.must.be.false();
   });
 
   it('must ignore self closing slash', async function() {
     const htmlDiffer = new HtmlDiffer({ ignoreSelfClosingSlash: true });
-    const files = readFiles('strip-self-closing-slash');
+    const files = await readFiles('strip-self-closing-slash');
+    const isEqual = await htmlDiffer.isEqual(...files);
 
-    (await htmlDiffer.isEqual(files.html1, files.html2)).must.be.true();
+    isEqual.must.be.true();
   });
 
   it('must throw an error for invalid preset', function() {
